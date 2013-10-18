60c4f5f53ff9ea6eee7ded4384681ddb7746fe6c
twisted1919 <totyoserbangeorgecristian@yahoo.com>
2013-10-18 17:41:25 +0400

    changes

---
+++ MailWizzApi/Endpoint/ListSegments.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file contains the list segments endpoint for MailWizzApi PHP-SDK.
+ * 
+ * @author Serban George Cristian <cristian.serban@mailwizz.com>
+ * @link http://www.mailwizz.com/
+ * @copyright 2013 http://www.mailwizz.com/
+ * @license http://www.mailwizz.com/api-client-license/
+ */
+ 
+ 
+/**
+ * MailWizzApi_Endpoint_ListSegments handles all the API calls for handling the list segments.
+ * 
+ * @author Serban George Cristian <cristian.serban@mailwizz.com>
+ * @package MailWizzApi
+ * @subpackage Endpoint
+ * @since 1.0
+ */
+class MailWizzApi_Endpoint_ListSegments extends MailWizzApi_Base
+{
+	/**
+	 * Get segments from a certain mail list
+	 * 
+	 * Note, the results returned by this endpoint can be cached.
+	 * 
+	 * @param string $listUid
+	 * @param integer $page
+	 * @param integer $perPage
+	 * @return MailWizzApi_Http_Response
+	 */
+	public function getSegments($listUid, $page = 1, $perPage = 10)
+	{
+		$client = new MailWizzApi_Http_Client(array(
+			'method' 		=> MailWizzApi_Http_Client::METHOD_GET,
+			'url' 			=> $this->config->getApiUrl(sprintf('lists/%s/segments', $listUid)),
+			'paramsGet'		=> array(
+				'page'		=> (int)$page, 
+				'per_page'	=> (int)$perPage
+			),
+			'enableCache'	=> true,
+		));
+		
+		return $response = $client->request();
+	}
+}
