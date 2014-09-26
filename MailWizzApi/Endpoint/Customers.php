659e2947eea3e9a8fddf72785f6e2cb680118add
twisted1919 <totyoserbangeorgecristian@yahoo.com>
2014-09-26 09:21:07 +0000

    customers endpoint and example

---
+++ MailWizzApi/Endpoint/Customers.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file contains the lists endpoint for MailWizzApi PHP-SDK.
+ * 
+ * @author Serban George Cristian <cristian.serban@mailwizz.com>
+ * @link http://www.mailwizz.com/
+ * @copyright 2013-2014 http://www.mailwizz.com/
+ */
+ 
+ 
+/**
+ * MailWizzApi_Endpoint_Customers handles all the API calls for customers.
+ * 
+ * @author Serban George Cristian <cristian.serban@mailwizz.com>
+ * @package MailWizzApi
+ * @subpackage Endpoint
+ * @since 1.0
+ */
+class MailWizzApi_Endpoint_Customers extends MailWizzApi_Base
+{
+    /**
+     * Create a new mail list for the customer
+     * 
+     * The $data param must contain following indexed arrays:
+     * -> customer
+     * -> company
+     * 
+     * @param array $data
+     * @return MailWizzApi_Http_Response
+     */
+    public function create(array $data)
+    {
+        if (isset($data['customer']['password'])) {
+            $data['customer']['confirm_password'] = $data['customer']['password'];
+        }
+        
+        if (isset($data['customer']['email'])) {
+            $data['customer']['confirm_email'] = $data['customer']['email'];
+        }
+        
+        if (empty($data['customer']['timezone'])) {
+            $data['customer']['timezone'] = 'UTC';
+        }
+        
+        $client = new MailWizzApi_Http_Client(array(
+            'method'        => MailWizzApi_Http_Client::METHOD_POST,
+            'url'           => $this->config->getApiUrl('customers'),
+            'paramsPost'    => $data,
+        ));
+        
+        return $response = $client->request();
+    }
+}
---
+++ examples/customers.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file contains examples for using the MailWizzApi PHP-SDK.
+ *
+ * @author Serban George Cristian <cristian.serban@mailwizz.com>
+ * @link http://www.mailwizz.com/
+ * @copyright 2013-2014 http://www.mailwizz.com/
+ */
+ 
+// require the setup which has registered the autoloader
+require_once dirname(__FILE__) . '/setup.php';
+
+// CREATE THE ENDPOINT
+$endpoint = new MailWizzApi_Endpoint_Customers();
+/*===================================================================================*/
+
+// CREATE CUSTOMER
+$response = $endpoint->create(array(
+    'customer' => array(
+        'first_name' => 'John',
+        'last_name'  => 'Doe',
+        'email'      => 'john.doe@doe.com',
+        'password'   => 'superDuperPassword',
+        'timezone'   => 'UTC',
+    ),
+    // company is optional, unless required from app settings
+    'company'  => array(
+        'name'     => 'John Doe LLC',
+        'country'  => 'United States', // see the countries endpoint for available countries and their zones
+        'zone'     => 'New York', // see the countries endpoint for available countries and their zones
+        'city'     => 'Brooklyn',
+        'zip_code' => 11222,
+        'address_1'=> 'Some Address',
+        'address_2'=> 'Some Other Address',
+    ),
+));
+
+// DISPLAY RESPONSE
+echo '<hr /><pre>';
+print_r($response->body);
+echo '</pre>';
