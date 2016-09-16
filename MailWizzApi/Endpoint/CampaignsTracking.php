6d8eac5208520bcac63cf51010460f40ddffe877
twisted1919 <totyoserbangeorgecristian@yahoo.com>
2016-09-16 15:42:21 +0300

    added the campaigns tracking endpoint which works with mailwizz >= 1.3.7.3

---
+++ MailWizzApi/Endpoint/CampaignsTracking.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file contains the campaigns endpoint for MailWizzApi PHP-SDK.
+ * 
+ * @author Serban George Cristian <cristian.serban@mailwizz.com>
+ * @link http://www.mailwizz.com/
+ * @copyright 2013-2015 http://www.mailwizz.com/
+ */
+ 
+ 
+/**
+ * MailWizzApi_Endpoint_CampaignsTracking handles all the API calls for campaigns.
+ * 
+ * @author Serban George Cristian <cristian.serban@mailwizz.com>
+ * @package MailWizzApi
+ * @subpackage Endpoint
+ * @since 1.0
+ */
+class MailWizzApi_Endpoint_CampaignsTracking extends MailWizzApi_Base
+{
+    /**
+     * Track campaign url click for certain subscriber 
+     *
+     * @param string $campaignUid
+     * @param string $subscriberUid
+     * @param string $hash
+     * @return MailWizzApi_Http_Response
+     */
+    public function trackUrl($campaignUid, $subscriberUid, $hash)
+    {
+        $client = new MailWizzApi_Http_Client(array(
+            'method'        => MailWizzApi_Http_Client::METHOD_GET,
+            'url'           => $this->config->getApiUrl(sprintf('campaigns/%s/track-url/%s/%s', (string)$campaignUid, (string)$subscriberUid, (string)$hash)),
+            'paramsGet'     => array(),
+        ));
+        
+        return $response = $client->request();
+    }
+
+    /**
+     * Track campaign open for certain subscriber
+     *
+     * @param string $campaignUid
+     * @param string $subscriberUid
+     * @return MailWizzApi_Http_Response
+     */
+    public function trackOpening($campaignUid, $subscriberUid)
+    {
+        $client = new MailWizzApi_Http_Client(array(
+            'method'        => MailWizzApi_Http_Client::METHOD_GET,
+            'url'           => $this->config->getApiUrl(sprintf('campaigns/%s/track-opening/%s', (string)$campaignUid, (string)$subscriberUid)),
+            'paramsGet'     => array(),
+        ));
+
+        return $response = $client->request();
+    }
+}
---
+++ examples/campaigns_tracking.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file contains examples for using the MailWizzApi PHP-SDK.
+ *
+ * @author Serban George Cristian <cristian.serban@mailwizz.com>
+ * @link http://www.mailwizz.com/
+ * @copyright 2013-2015 http://www.mailwizz.com/
+ */
+ 
+// require the setup which has registered the autoloader
+require_once dirname(__FILE__) . '/setup.php';
+
+// PLEASE NOTE THAT THIS ENDPOINT ONLY WORKS WITH MAILWIZZ >= 1.3.7.3
+// CREATE THE ENDPOINT
+$endpoint = new MailWizzApi_Endpoint_CampaignsTracking();
+
+/*===================================================================================*/
+
+// Track subscriber click for campaign click
+$response = $endpoint->trackUrl('CAMPAIGN-UNIQUE-ID', 'SUBSCRIBER-UNIQUE-ID', 'URL-HASH');
+
+// DISPLAY RESPONSE
+echo '<hr /><pre>';
+print_r($response->body);
+echo '</pre>';
+
+/*===================================================================================*/
+
+// Track subscriber open for campaign
+$response = $endpoint->trackOpening('CAMPAIGN-UNIQUE-ID', 'SUBSCRIBER-UNIQUE-ID');
+
+// DISPLAY RESPONSE
+echo '<hr /><pre>';
+print_r($response->body);
+echo '</pre>';
